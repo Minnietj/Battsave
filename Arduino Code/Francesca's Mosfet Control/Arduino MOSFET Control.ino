79c49780c83327dd74ba7c7b6c7512b271334668
fcrema <32024886+fcrema@users.noreply.github.com>
2019-03-10 17:46:51 -0700

    Update Arduino MOSFET Control.ino

--- Arduino Code/Francesca's Mosfet Control/Arduino MOSFET Control.ino
+++ Arduino Code/Francesca's Mosfet Control/Arduino MOSFET Control.ino
@@ -1,29 +1,37 @@
 //very basic code to test out the arduino's ability to work through the MOSFET. Will greatly update with testing and when working with actual data
 
+vint analogPin = A0; 
+  int v = 0; //voltage of battery
 void setup() {
-// initialize digital pin 3 as an output.
+// initialize digital pin 13 as an output.
 pinMode(3, OUTPUT);
+  Serial.begin(9600);       
+  //  setup serial
+
 }
 // the loop function runs over and over again forever
 void loop() {
 
- int v; //voltage of battery
- int thresh ; //low threshold
  
- //commented out code was to test LED control
+int thresh = 100 ; //low threshold
 /*digitalWrite(3, HIGH); // turn the LED on (HIGH is the voltage level)
 delay(1000); // wait for a second
 digitalWrite(3, LOW); // turn the LED off by making the voltage LOW
 delay(1000);  // wait for a second */
 
+
+  v = analogRead(analogPin);  // read the input pin
+  Serial.println(v);          // debug value
+
 if(v >= thresh){
 
-  digitalWrite(3, HIGH);
+  digitalWrite(3, LOW);
   
 }
 else{
 
-  digitalWrite(3, LOW); 
+  digitalWrite(3, HIGH); 
 }
 }
+
 
