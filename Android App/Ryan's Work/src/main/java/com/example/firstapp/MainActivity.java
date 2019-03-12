5f875f561921b2b140ba3611ca5bf9312ff22e5c
dkjklmn <48234578+dkjklmn@users.noreply.github.com>
2019-03-12 12:07:16 -0700

    Add files via upload

---
+++ Android App/Ryan's Work/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.firstapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toastMe(View view){
+        Toast myToast = Toast.makeText(this, "Toast me",
+                Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
+    /*public void countMe (View view) {
+        // Get the text view
+        TextView showCountTextView =
+                (TextView) findViewById(R.id.textView);
+        // Get the value of the text view.
+        String countString= showCountTextView.getText().toString();
+        // Convert value to a number and increment it
+        Integer count = Integer.parseInt(countString);
+        count++;
+        // Display the new value in the text view.
+        showCountTextView.setText(count.toString());
+    }
+    */
+
+    public void percentMe(View view) {
+        float batteryPct = 0;
+        double StopCharge = 80.0;
+        while (batteryPct < StopCharge) {
+            IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+            //Are we charging/charged?
+            int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                    status == BatteryManager.BATTERY_STATUS_FULL;
+            //How are we charging?
+            int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+            boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+            boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+            //BATTERY PERCENTAGE
+            int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+            batteryPct = (level / (float) scale) * 100;
+            TextView randomView = (TextView)
+                    findViewById(R.id.textViewPERCENTAGE);
+
+            String mytext = Float.toString(batteryPct);
+            Thread.sleep(1000);
+                    //.SECONDS.wait(5000);
+            randomView.setText(mytext);
+        }
+    }
+
+    public void voltMe(View view){
+        int count = 0;
+        while (count < 10) {
+
+            IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+            //Are we charging/charged?
+            int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                    status == BatteryManager.BATTERY_STATUS_FULL;
+            //How are we charging?
+            int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+            boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+            boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+            int batteryVoltage = batteryStatus.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+            TextView randomView = (TextView)
+                    findViewById(R.id.textViewTEMPERATURE);
+
+            randomView.setText(Integer.toString(batteryVoltage));
+            count++;
+
+        }
+    }
+
+    public void ampMe(View view) {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+        //Are we charging/charged?
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS,-1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+        //How are we charging?
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+        TextView randomView = (TextView)
+                findViewById(R.id.textViewCURRENT);
+
+        int batteryAmp = BatteryManager.BATTERY_PROPERTY_CURRENT_NOW;
+
+        randomView.setText(Integer.toString(batteryAmp));
+    }
+
+    public void tempMe(View view) {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+        //Are we charging/charged?
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+        //How are we charging?
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+        TextView randomView = (TextView)
+                findViewById(R.id.textViewVOLTAGE);
+
+        int batteryTemperature = batteryStatus.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+        randomView.setText(Integer.toString(batteryTemperature));
+    }
+}
