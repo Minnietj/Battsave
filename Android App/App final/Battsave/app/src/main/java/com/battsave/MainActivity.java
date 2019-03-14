da46922f62f5d303e03e926668bfdce2d3333a65
Taylor <minnietj@hotmail.com>
2019-03-13 22:03:43 -0700

    Added Final Versions

---
+++ Android App/App final/Battsave/app/src/main/java/com/battsave/MainActivity.java
@@ -0,0 +1,252 @@
+package com.battsave;
+
+//Uses Library! https://github.com/OmarAflak/Arduino-Library
+//Library downloaded through Gradle
+
+import android.hardware.usb.UsbDevice;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+
+import java.util.concurrent.TimeUnit;
+
+import android.view.View;
+
+import me.aflak.arduino.Arduino;
+import me.aflak.arduino.ArduinoListener;
+
+
+public class MainActivity extends AppCompatActivity {
+    Arduino arduino;
+    Handler handler;
+    TextView myTextView;
+    int Temperature = 50;
+    double Voltage = 2.7;
+    double Current = 1.2;
+    double Percentage = 70;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Code below acts as listener or interrupt handler. Runs in parallel with main loop.
+        arduino.setArduinoListener(new ArduinoListener() {
+            String buffer = new String();
+
+            @Override
+            public void onArduinoAttached(UsbDevice device) {
+                arduino.open(device);
+            }
+
+            @Override
+            public void onArduinoDetached() {
+                // arduino detached from phone
+            }
+
+            @Override
+            public void onArduinoMessage(byte[] bytes) {
+                //This Code runs whenever phone receives a message from Arduino
+
+                //Stores whatever was received in string "message"
+                String message = new String(bytes);
+
+                //Concatenate new message string into existing buffer
+                buffer = buffer + message;
+
+
+                //This code extracts data from buffer by detecting null characters (\0). When data is extracted, The buffer is cleared, then printed to a textview, or you can do whatever you want with it.
+                if (buffer.contains("\0") && buffer.indexOf("\0") > 0 && buffer.length() > 0) {
+                    //extract string
+                    Log.d("on recieve", buffer); //checkpoint print to logcat
+                    final String print = buffer.substring(0, buffer.indexOf("\0"));
+                    Log.d("this is print", print); //checkpoint print to logcat
+                    //clear buffer
+                    buffer = "";
+                    Log.d("buffer after deleted", buffer); //checkpoint print to logcat
+                    //Print to textview
+                    runOnUiThread(new Runnable() {
+                        //this is in a new runnable because the Arduino listener does not have access to textviews.
+                        @Override
+                        public void run() {
+
+                            myTextView.setText(print);
+                            // Stuff that updates the UI
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onArduinoOpened() {
+                // you can start the communication
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        //This is just a message to indicate that comms have been established between arduino and phone
+                        myTextView.setText("yoo my dude its half working");
+                        // Stuff that updates the UI
+
+                    }
+                });
+                String str = "Hello Arduino !";
+                arduino.send(str.getBytes());
+            }
+
+            @Override
+            public void onUsbPermissionDenied() {
+                // Permission denied, display popup then
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        //Sad message.
+                        myTextView.setText("Permission DENIED bitchhhh");
+                        // Stuff that updates the UI
+
+                    }
+                });
+                arduino.reopen();
+            }
+        });
+
+
+    }
+
+
+    public void percentMe() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+        //Are we charging/charged?
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+        //How are we charging?
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+        //BATTERY PERCENTAGE
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        //Percentage = (level /  scale) * 100;
+        Percentage = (level*100) / (scale);
+
+        TextView randomView = (TextView)
+                findViewById(R.id.textViewPERCENTAGE);
+
+        String mytext = Double.toString(Percentage);
+        randomView.setText(mytext);
+    }
+
+    public void voltMe() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+        //Are we charging/charged?
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+        //How are we charging?
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+        Voltage = batteryStatus.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+        TextView randomView = (TextView)
+                findViewById(R.id.textViewVOLTAGE);
+
+        randomView.setText(Double.toString(Voltage));
+
+    }
+
+    public void ampMe() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+        //Are we charging/charged?
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+        //How are we charging?
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+        TextView randomView = (TextView)
+                findViewById(R.id.textViewCURRENT);
+
+        Current = BatteryManager.BATTERY_PROPERTY_CURRENT_NOW;
+
+        randomView.setText(Double.toString(Current));
+    }
+
+    public void tempMe() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+        //Are we charging/charged?
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+        //How are we charging?
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+        TextView randomView = (TextView)
+                findViewById(R.id.textViewTEMPERATURE);
+
+        Temperature = batteryStatus.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+        randomView.setText(Integer.toString(Temperature));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Do this stuff when the app is initially launched
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arduino = new Arduino(this); //make a new arduino object
+        arduino.addVendorId(6790); //Vendor id lets phone know that connected thing is indeed an arduino
+        myTextView = (TextView) findViewById(R.id.Textbox1); //associate myTextView object with actuial textview in layout
+        handler = new Handler(); //idk
+
+
+        //Code below acts as main loop
+        final Runnable r = new Runnable() {
+            public void run() {
+                percentMe();
+                voltMe();
+                ampMe();
+                tempMe();
+
+                if (arduino.isOpened()) { //Only send a message if comms with the arduino have been established
+                    //Format Message to send
+                    String str = Temperature + "\n" + Voltage + "\n" + Current + "\n" + Percentage + "\n" + "ABCDEFG" + "\0";
+                    arduino.send(str.getBytes());
+                } else { //if arduino is not connected
+                    myTextView.setText("I cant find an arduino :(");
+                }
+
+                handler.postDelayed(this, 2000);
+            }
+        };
+
+        handler.postDelayed(r, 2000);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        arduino.unsetArduinoListener();
+        arduino.close();
+    }
+}
---
+++ Arduino Code/Final Code/Battsave.ino
@@ -0,0 +1,235 @@
+
+  #include <SPI.h>
+  #include <Wire.h>
+  #include <Adafruit_GFX.h>
+  #include <Adafruit_SSD1306.h>
+  #define SCREEN_WIDTH 128 // OLED display width, in pixels
+  #define SCREEN_HEIGHT 32 // OLED display height, in pixels
+  // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+  #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+  Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+
+    //Global Variables for Incoming Serial - Functions are designed in such a way that variables are stored globally. 
+    //I dont forsee this causing any issues, but if you suspect it is, lemme knmow and i might be able to change it to passing variables.
+    
+    double Temperature = 0;
+    double Voltage = 0;
+    double Current = 0;
+    double Percentage = 0;
+    
+    String incoming;
+    String message;
+    boolean newData = false;
+
+    int analogPin = A0; 
+    int Threshold = 6;
+
+    const int upButtonPin = 4;
+    const int downButtonPin = 5;
+
+    int loopCounter = 0;
+
+void setup() {
+    pinMode(3, OUTPUT);
+    pinMode(4, INPUT);
+    pinMode(5, INPUT);
+    Serial.begin(9600);
+
+    //Display Setup
+     if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+     Serial.println(F("SSD1306 allocation failed"));
+     for(;;); // Don't proceed, loop forever
+     }
+     // Show initial display buffer contents on the screen --
+     // the library initializes this with an Adafruit splash screen.
+     display.display();
+     delay(2000); // Pause for 2 seconds
+     // Clear the buffer
+     display.clearDisplay();
+}
+
+void UpdateScreen(){
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  // Not all the characters will fit on the display. This is normal.
+  // Library will draw what it can and the rest will be clipped.
+  //for(int16_t i=0; i<256; i++) {
+  //  if(i == '\n') display.write(' ');
+  //  else          display.write(i);
+  //}
+  
+   
+  display.print("Temp: ");
+  display.println(Temperature);
+  display.print("Volt: ");
+  display.println(Voltage);
+  display.print("Amp: ");
+  display.println(Current);
+  display.print("Prcnt: ");
+  display.println(Percentage);
+  display.display();
+
+  
+}
+
+
+
+
+//Takes raw incoming serial data from buffer, separates it into individual messages upon detection of null character
+//Also triggers newData flag to signal that the serial data has arrived and we should now update our variables.
+void getMessage(){
+  
+  if(Serial.available()){
+        
+        char c = Serial.read();
+        String test = "test";
+
+        if (c!= '\0'){ 
+          incoming += c;
+          //Serial.print("recorded");
+        }
+        if (c == '\0'){
+          //Serial.print("full message received: ");
+          //Serial.print(incoming);
+          message = incoming;
+          incoming = "";
+          newData = true;
+        }
+    }
+}
+
+
+//Takes messages from getMessage() and parses them into their respective variables
+//Also clears the newData flag
+void parseMessage(){
+  //Structure of message this function is designed to parse:
+  //Temp + \n + Voltage + \n + Current + \n + Percentage + \n
+  
+  String temporary = "";
+  int i = 0;
+  int varCounter = 0; //increments to switch between vars
+ 
+    
+    for (i=0; i < message.length(); i++){
+      if (message[i] != '\n'){
+        temporary += message [i];
+        //Serial.println(message[i]);
+      }
+      
+      if (message[i] == '\n'){
+        if(varCounter == 0){
+          Temperature = temporary.toDouble();
+          varCounter++;
+        }
+        else if(varCounter == 1){
+          Voltage = temporary.toDouble();
+          varCounter++;
+        }
+        else if(varCounter == 2){
+          Current = temporary.toDouble();
+          varCounter++;
+        }
+        else if(varCounter == 3){
+          Percentage = temporary.toDouble();
+          varCounter++;
+        }
+        temporary = "";
+        
+      }
+    }
+  
+
+  
+  newData = false;
+  
+}
+
+
+void loop() {
+  
+  getMessage(); //if there is any incoming serial to record, record it.
+  if (newData == true){
+    parseMessage(); //if a full message has been recieved, parse and extract the data from it.
+
+    // To test on PC serial monitor: exchange '\0' in getMessage() for '\r' , uncomment below
+    /* 
+    Serial.println("Temperature: ");
+    Serial.println(Temperature);
+    Serial.println("Voltage: ");
+    Serial.println(Voltage);
+    Serial.println("Current: ");
+    Serial.println(Current);
+    Serial.println("Percentage: ");
+    Serial.println(Percentage);
+    */
+  }
+
+
+  //Rest of our code
+
+
+  //Button Read
+
+  if((digitalRead(upButtonPin)) && (Threshold<100) && (loopCounter == 0)){
+    Threshold++;
+    Serial.println("up");
+  }
+  if((digitalRead(downButtonPin))&& (Threshold>10) && (loopCounter == 0)){
+    Threshold--;
+    Serial.println("down");
+  }
+
+
+
+
+
+  //MOSFET Control
+  if(Percentage >= Threshold){
+    digitalWrite(3, HIGH);
+  }
+  else{
+    if (Percentage <= (Threshold-5)){ //Implemented Hysteresis
+      digitalWrite(3, LOW); 
+    }
+  }
+
+  //Serial.println(Threshold);
+  UpdateScreen();
+  
+   loopCounter++;
+   if (loopCounter == 133){
+   loopCounter = 0;
+   }
+   
+
+//BE WARY OF USING DELAYS
+//Arduinos can only run one loop. This means that they can only do one thing at a time. 
+//If the code tells them to delay for 5 seconds, any incoming serial messages from the phone might be ignored and missed.
+//(I believe theres actually a hardware buffer to help mitigate this problem, but it won't eliminate it)
+//Idk why we would need to use delays anywhere in this code, but if we do, we will need to implement an interrupt handler :(
+  
+}
+
+
+
+
+/*
+    //Echoes back to Phone. Used for testing purposes, will likely not include in final product.
+    if(Serial.available()){
+        char c = Serial.read();
+        Serial.print(c);
+    }
+*/
+
+
+
+
+
+    
