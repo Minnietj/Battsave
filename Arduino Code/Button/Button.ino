16a2609b5fe707ea0732d6fc72e43a658d3d4c56
dkjklmn <48234578+dkjklmn@users.noreply.github.com>
2019-03-14 11:49:42 -0700

    Add files via upload

--- Arduino Code/Button/Button.ino
+++ Arduino Code/Button/Button.ino
@@ -30,11 +30,15 @@ const int buttonPinDown = 3;    // This button decreases number
 int buttonStateUp = 0;         // variable for reading the pushbutton status
 int buttonStateDown = 0;
 int number = 80;            //number that increases/decreases
+int lastButtonStateUp = 0;
+int lastButtonStateDown = 0;
+int buttonPushCounterUp = 0;
+int buttonPushCounterDown = 9;
 
 void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
-  
+  Serial.begin(9600);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPinUp, INPUT);
   pinMode(buttonPinDown, INPUT);
@@ -44,21 +48,29 @@ void loop() {
   // read the state of the pushbutton value:
   buttonStateUp = digitalRead(buttonPinUp);
   buttonStateDown = digitalRead(buttonPinDown);
-  
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonStateUp == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, LOW);
-    number = number + 1;
-  } 
 
-  else if (buttonStateDown == HIGH){
-    digitalWrite(ledPin, LOW);
-    number = number - 1;
-  }
-  
-  else {
-    // turn LED off:
-    digitalWrite(ledPin, HIGH);
-  }
+
+  if(buttonStateUp != lastButtonStateUp){
+    
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    if (buttonStateUp == HIGH) {
+          number++;
+          Serial.print(number);
+          Serial.print("\n");
+           
+    }    
+ }
+  if(buttonStateDown != lastButtonStateDown){
+    
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    if (buttonStateDown == HIGH) {
+          number--;
+          //buttonPushCounterUp++;
+          Serial.print(number);
+          Serial.print("\n");
+          
+    }    
+ }
+  lastButtonStateUp = buttonStateUp;
+  lastButtonStateDown = buttonStateDown;
 }
